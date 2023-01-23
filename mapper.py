5bcd5c66385a2c6d88cc7a1d50fdb9aa6d026cc4
Bri McCoy <bmccoy002@regis.edu>
2023-01-23 04:18:39 +0000

    added 2 more stopwords

--- mapper.py
+++ mapper.py
@@ -5,10 +5,15 @@ import sys
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    #makes every word lowercase
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    stopwords = set(['the','and'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+            print '%s\t%s' % (word.lower(), "1")
